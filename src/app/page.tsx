334ee1e05928312ba8873c0185318c6d711cf46c
Yash Nirala <ysh.nrl2@gmail.com>
2023-11-22 18:10:00 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,9 @@ const Home = () => {
         </main>
         
         <section className="fixed ml-[890px] w-[350px]">
+          <div className='mb-5'></div>
           <h1>Trending Section</h1>
-          <div className='text-[10px] '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo quibusdam harum magnam! Eos iste nulla minus voluptatum, a quis consequuntur earum ipsam nihil, maxime ad sapiente laboriosam rerum vel dolorem magni veniam odio labore architecto eligendi animi non et. At quam dolores dignissimos sint ducimus sunt id. Assumenda enim qui esse iste, animi quo recusandae totam deserunt quae adipisci fugiat perspiciatis laboriosam sed necessitatibus, iure eligendi atque eos voluptatum, consequatur amet ratione aliquam placeat! Assumenda pariatur error reiciendis ut sapiente, velit maxime et delectus eos ipsam, mollitia soluta cum temporibus laudantium est aliquam quod quas eius suscipit hic odio debitis?</div>
+          <div className='text-[10px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo quibusdam harum magnam! Eos iste nulla minus voluptatum, a quis consequuntur earum ipsam nihil, maxime ad sapiente laboriosam rerum vel dolorem magni veniam odio labore architecto eligendi animi non et. At quam dolores dignissimos sint ducimus sunt id. Assumenda enim qui esse iste, animi quo recusandae totam deserunt quae adipisci fugiat perspiciatis laboriosam sed necessitatibus, iure eligendi atque eos voluptatum, consequatur amet ratione aliquam placeat! Assumenda pariatur error reiciendis ut sapiente, velit maxime et delectus eos ipsam, mollitia soluta cum temporibus laudantium est aliquam quod quas eius suscipit hic odio debitis?</div>
         </section>
 
       </div>
